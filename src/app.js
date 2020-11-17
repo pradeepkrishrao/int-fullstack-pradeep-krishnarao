d627da008a4184f32dfb8318bcaca877b7d62727
pradeepkrishrao <56447418+pradeepkrishrao@users.noreply.github.com>
2020-11-18 00:51:13 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import {render} from 'react-dom';
 import { Router, Route, IndexRoute, browserHistory } from 'react-router';
-import LoginPage from './components/auth/LoginPage';
+import LoginPage from './client/auth/LoginPage';
 
-import Layout from './components/common/Layout';
-import MoviesPage from './components/movies/MoviesPage';
-import TVShowsPage from './components/tvshows/TVShowsPage';
-import WatchlistPage from './components/watchlist/WatchlistPage';
+import Layout from './client/common/Layout';
+import MoviesPage from './server/movies/MoviesPage';
+import TVShowsPage from './server/tvshows/TVShowsPage';
+import WatchlistPage from './server/watchlist/WatchlistPage';
 import '../scss/site.scss';
 
 render(
