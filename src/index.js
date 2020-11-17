ca5d9f772915641bcd2f5a599bb6460b6b7f0607
pradeepkrishrao <56447418+pradeepkrishrao@users.noreply.github.com>
2020-11-18 01:00:09 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,12 +6,13 @@
   <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>TheMovieDB app</title>
+    <title>Fireworks Entertainment App</title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://ok1static.oktacdn.com/assets/js/sdk/okta-signin-widget/2.3.0/css/okta-sign-in.min.css" type="text/css" rel="stylesheet"/>
     <!-- Theme file: Customize or replace this file if you want to override our default styles -->
     <link href="https://ok1static.oktacdn.com/assets/js/sdk/okta-signin-widget/2.3.0/css/okta-theme.css" type="text/css" rel="stylesheet"/>
+    <link rel="stylesheet" href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css"
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css">
   </head>
   <body>
