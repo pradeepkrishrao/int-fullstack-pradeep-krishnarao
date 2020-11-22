59fd1128b6952708d6be80e108a36579feed279b
pradeepkrishrao <56447418+pradeepkrishrao@users.noreply.github.com>
2020-11-23 02:51:35 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -2,12 +2,10 @@ import React from 'react';
 import {render} from 'react-dom';
 import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 import LoginPage from './client/auth/LoginPage';
-
 import Layout from './client/common/Layout';
 import MoviesTVPage from './server/movies/MoviesTVPage';
 import DetailsPage from './server/details/DetailsPage';
 import ResultsPage from './client/results/ResultsPage';
-
 import '../scss/site.scss';
 
 render(
