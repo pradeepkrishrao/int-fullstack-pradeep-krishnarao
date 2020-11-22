39092f7d8a700e7fa803c9c7a31128bba5d48258
pradeepkrishrao <56447418+pradeepkrishrao@users.noreply.github.com>
2020-11-23 00:20:44 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -4,19 +4,20 @@ import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 import LoginPage from './client/auth/LoginPage';
 
 import Layout from './client/common/Layout';
-import MoviesPage from './server/movies/MoviesPage';
-import TVShowsPage from './server/tvshows/TVShowsPage';
-import WatchlistPage from './server/watchlist/WatchlistPage';
+import MoviesTVPage from './server/movies/MoviesTVPage';
+import DetailsPage from './server/details/DetailsPage';
+import ResultsPage from './client/results/ResultsPage';
+
 import '../scss/site.scss';
 
 render(
   <Router history={browserHistory}>
     <Route path="/" component={Layout}>
-      <IndexRoute component={MoviesPage}/>
-      <Route path="/tvshows" component={TVShowsPage}/>
-      <Route path="/watchlist" component={WatchlistPage}/>
+      <IndexRoute component={MoviesTVPage}/>
+      <Route path="/details" component={DetailsPage}/>
+      <Route path="/results" component={ResultsPage}/>
       <Route path="/login" component={LoginPage}/>
     </Route>
-</Router>,
+  </Router>,
   document.getElementById('app')
 )
