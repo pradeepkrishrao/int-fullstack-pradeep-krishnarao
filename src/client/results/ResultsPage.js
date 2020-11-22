0ca127065eb79ab8d6d97925aea0efa7dad4d679
pradeepkrishrao <56447418+pradeepkrishrao@users.noreply.github.com>
2020-11-23 02:28:49 +0530

    Update ResultsPage.js

--- src/client/results/ResultsPage.js
+++ src/client/results/ResultsPage.js
@@ -4,20 +4,6 @@ class ResultsPage extends Component{
 render(){
 return(
 <div className="container">
-<header className="px-4 py-3 shadow bg-blue-900">
-      <div className="container mx-auto">
-        <h1
-          className="text-2xl text-gray-100 tracking-wider font-bold leading-none"
-        >
-        Fireworks
-        </h1>
-        <h2
-          className="text-sm text-gray-300 tracking-widest font-semibold uppercase leading-none"
-        >
-        Entertainment
-        </h2>
-      </div>
-    </header>
 <h1>Resultlist of movies/tv shows</h1>
 </div>
 )
