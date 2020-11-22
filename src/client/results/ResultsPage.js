efa3c606a32df3d093b8f021fb2cd608509bf5ce
pradeepkrishrao <56447418+pradeepkrishrao@users.noreply.github.com>
2020-11-22 23:47:46 +0530

    Update ResultsPage.js

--- src/client/results/ResultsPage.js
+++ src/client/results/ResultsPage.js
@@ -21,8 +21,7 @@ class ResultsPage extends Component{
       <h1>Resultslist of movies/tv shows</h1>
       <div className="container mx-auto pb-4">
       <div className="max-w-sm w-full lg:max-w-full lg:flex mt-4">
-      
-<div
+      <div
           className="h-48 lg:h-auto lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden"
           style="
             background-image: url('https://fwemoviedb.herokuapp.com/img/w188_and_h282_bestv2/6FfCtAuVAW8XJjZ7eWeLibRLWTw.jpg');
@@ -36,7 +35,7 @@ class ResultsPage extends Component{
             <div className="text-gray-900 font-bold text-xl mb-2">
               Star Wars
             </div>
-<p className="text-gray-700 text-base">
+            <p className="text-gray-700 text-base">
               Princess Leia is captured and held hostage by the evil Imperial
               forces in their effort to take over the galactic Empire.
               Venturesome Luke Skywalker and dashing captain Han Solo team
@@ -65,7 +64,7 @@ class ResultsPage extends Component{
               Star Trek
             </div>
 
-<p className="text-gray-700 text-base">
+            <p className="text-gray-700 text-base">
               The fate of the galaxy rests in the hands of bitter rivals. One,
               James Kirk, is a delinquent, thrill-seeking Iowa farm boy. The
               other, Spock, a Vulcan, was raised in a logic-based society that
@@ -79,8 +78,7 @@ class ResultsPage extends Component{
       </div>
 
       <div className="max-w-sm w-full lg:max-w-full lg:flex mt-4">
-
-<div
+      <div
           className="h-48 lg:h-auto lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden"
           style="
             background-image: url('https://fwemoviedb.herokuapp.com/img/w188_and_h282_bestv2/db32LaOibwEliAmSL2jjDF6oDdj.jpg');
@@ -94,8 +92,7 @@ class ResultsPage extends Component{
             <div className="text-gray-900 font-bold text-xl mb-2">
               Star Wars: The Rise of Skywalker
             </div>
-
-<p className="text-gray-700 text-base">
+            <p className="text-gray-700 text-base">
               The surviving Resistance faces the First Order once again as the
               journey of Rey, Finn and Poe Dameron continues. With the power and
               knowledge of generations behind them, the final battle begins...
@@ -106,7 +103,6 @@ class ResultsPage extends Component{
           </div>
         </div>
       </div>
-
       <div className="max-w-sm w-full lg:max-w-full lg:flex mt-4">
         <div
           className="h-48 lg:h-auto lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden"
@@ -118,8 +114,7 @@ class ResultsPage extends Component{
         <div
           className="border-r border-b border-l border-gray-400 lg:border-l-0 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal"
         >
-
-<div className="mb-8">
+        <div className="mb-8">
             <div className="text-gray-900 font-bold text-xl mb-2">
               Star Trek: Nemesis
             </div>
@@ -137,10 +132,8 @@ class ResultsPage extends Component{
           </div>
         </div>
       </div>
-
       <div className="max-w-sm w-full lg:max-w-full lg:flex mt-4">
-
-<div
+      <div
           className="h-48 lg:h-auto lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden"
           style="
             background-image: url('https://fwemoviedb.herokuapp.com/img/w188_and_h282_bestv2/m7SHlvcGfCkbzk2xP7XHDOI6o93.jpg');
@@ -160,9 +153,8 @@ class ResultsPage extends Component{
               and everything the Federation stands for to the test.
             </p>
           </div>
-
-<div className="text-sm">
-            <p className="text-gray-600">Jul 7, 2016</p>
+          <div className="text-sm">
+          <p className="text-gray-600">Jul 7, 2016</p>
           </div>
         </div>
       </div>
