043a7b72400dfd3324b943e95ea2af8a6ffed9fc
pradeepkrishrao <56447418+pradeepkrishrao@users.noreply.github.com>
2020-11-23 00:16:57 +0530

    Update LoginPage.js

--- src/client/auth/LoginPage.js
+++ src/client/auth/LoginPage.js
@@ -8,7 +8,7 @@ export default class LoginPage extends React.Component{
         this.widget = new OktaSignIn({
             baseUrl: 'https://dev-905268.oktapreview.com',
             clientId: '0oagfe9x2agrYf7P50h7',
-            redirectUrl: 'https://https://fwemoviedb.herokupp.com/3/movie/populat?api_key=e800e93ef4806616964242bbd2619ae1&page=1',
+            redirectUrl: 'https://https://fwemoviedb.herokuapp.com/3/movie/popular?api_key=e800e93ef4806616964242bbd2619ae1&page=1',
             authParams: {
                 responseType: 'id_token'
             }
@@ -16,7 +16,9 @@ export default class LoginPage extends React.Component{
         this.showLogin = this.showLogin.bind(this);
         this.logout = this.logout.bind(this);
     }
-        componentDidMount(){
+    
+
+    componentDidMount(){
         this.widget.session.get((response)=>{
             if(response.status !== 'INACTIVE'){
                 this.setState({user:response.login});
@@ -48,7 +50,7 @@ export default class LoginPage extends React.Component{
             {this.state.user ? (
                 <div className="container">
                 <div>Hello, welcome!</div>
-                <button onClick={this.logout}>Logout</button>
+            <button onClick={this.logout}>Logout</button>
             </div>
             ): null}
             {this.state.user ? null : (
